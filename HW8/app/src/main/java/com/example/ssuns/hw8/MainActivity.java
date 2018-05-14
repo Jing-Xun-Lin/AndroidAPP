746799b47285ba8d6a514b652e76c2b9ebe5ad1c
Jing-Xun-Ling <t105590021@ntut.org.tw>
2018-05-14 20:42:13 +0800

    asdfasdfasdfas

--- HW8/app/src/main/java/com/example/ssuns/hw8/MainActivity.java
+++ HW8/app/src/main/java/com/example/ssuns/hw8/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.ssuns.hw8;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mitemSpn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mitemSpn = (Spinner) findViewById(R.id.itemSpn);
+
     }
 }
